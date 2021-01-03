783b34b2af3026812854a5fda322ddad53d07d2d
andre <jiuzhou.zhou9@gmail.com>
2021-01-04 00:10:43 +0900

    andre

---
+++ bk/mkCab.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+::makecab
+echo %date%%time% >> run.log 
+makecab /f D:\tmp\work\list.txt /d expresstype=mszip /d expressmemory=21 /d maxdisksize=1024000000 /d diskdirectorytemplate=D:\tmp\zipB\ /d cabinetnametemplate=20210312.cab
+echo %date%%time% >> run.log 
+pause
